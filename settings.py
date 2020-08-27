d320ae8db4e854e0f3ab7f828b799d346ac95ac8
Roger Mayer <rmayer1984@gmail.com>
2020-08-27 08:21:37 -0500

    added bullet class

--- settings.py
+++ settings.py
@@ -6,3 +6,8 @@ class Settings:
         self.screen_height = 700
         self.bg_color = (230, 230, 230)
         self.ship_speed = 2
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
